e091307dbfeecf35da8e25f19f435a896b8fcf0f
Fida <fida.ullah@targetofs.com>
2019-05-30 00:39:16 +0500

    Updating the API URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
 const API_VERSION = `v2`;
 
-export const API_END_POINT =
-  process.env.NODE_ENV === "development"
-    ? ""
-    : `https//api.foursquare.com/${API_VERSION}`;
+export const API_END_POINT = `//api.foursquare.com/${API_VERSION}`;
